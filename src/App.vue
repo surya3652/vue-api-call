b7e633ee9f63e04c701ad52f1ba853292c092ad4
robot <cgajula152@gmail.com>
2024-02-07 11:35:57 +0530

    Created a Vue app for demonstrating API Calls

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld
+      msg="Sample app for calling API End point for displaying data"
+      api="https://api.publicapis.org/entries "
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,66 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <br />
+    <a :href="api">Click Here to see content from API</a><br />
+    <p>Click here for toggling to populate the table</p>
+    <br /><button @click="trigger = !trigger">Go!</button>
+    <br />
+    <table v-if="trigger">
+      <tr>
+        <th>Index</th>
+        <th>API</th>
+        <th>Description</th>
+        <th>Auth</th>
+        <th>HTTPS</th>
+        <th>Cors</th>
+        <th>Link</th>
+        <th>Category</th>
+      </tr>
+      <tr v-for="(data, index) in api_info" :key="index">
+        <td>{{ index + 1 }}</td>
+        <td>{{ data.API }}</td>
+        <td>{{ data.Description }}</td>
+        <td>{{ data.Auth }}</td>
+        <td>{{ data.HTTPS }}</td>
+        <td>{{ data.Cors }}</td>
+        <td>
+          <a :href="data.Link">{{ data.Link }}</a>
+        </td>
+        <td>{{ data.Category }}</td>
+      </tr>
+    </table>
   </div>
 </template>
-
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      api_info: [],
+      trigger: false,
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+    api: String,
+  },
+  mounted() {
+    axios
+      .get(this.api)
+      .then((res) => (this.api_info = res.data.entries))
+      .catch((err) =>
+        console.log(`Unable to process the request due to ${err}`)
+      );
+  },
+  methods: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1,
 h3 {
   margin: 40px 0 0;
 }
@@ -53,6 +73,15 @@ li {
   margin: 0 10px;
 }
 a {
+  font-size: medium;
   color: #42b983;
+  margin-bottom: 10px;
+}
+table,
+th,
+tr,
+td {
+  padding: 2px;
+  border: 1px solid black;
 }
 </style>
